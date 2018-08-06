47be7b3913f4657e25d984dd79651a554b8d730d
mehulmomaya <mehulmomaya513@gmail.com>
2018-08-06 11:41:06 +0530

    Adding test cases

--- sweetiq_test/calculator.js
+++ sweetiq_test/calculator.js
@@ -1,11 +1,11 @@
 module.exports = {
   calculate: function(operator, num1, num2){
-    if (num1 == '' || num2== '' || operator == '') {
+    if (num1== null ||num2== null || operator == '') {
       return 'One or more of the inputs to the function is Empty / Null, Input format --> calculate('+', 4 7)';
     }
     if ((typeof(num1) != 'number') || (typeof(num2) != 'number')) {
       console.log(typeof(num1), typeof(num2));
-      return 'Invalid Input Numbers';
+      return 'Invalid Input Number';
     }
     if(operator != '-' && operator != '*' && operator != '/' && operator != '+' ){
       return 'Invalid Input Operator'
--- sweetiq_test/test/calculatorTest.js
+++ sweetiq_test/test/calculatorTest.js
@@ -3,6 +3,10 @@ const cal = require('../calculator.js').calculate;
 
 describe('Cal', function(){
 
+/*
+ *  Funtional Test Cases
+ */
+
   it('Addition', function(){
     let result = cal('+',3,5);
     assert.equal(result, 8);
@@ -57,28 +61,67 @@ describe('Cal', function(){
     assert.equal(result, -1.6666666666666667);
   });
 
-  it('Division with one Negative Numbers', function(){
-    let result = cal('+',5,-3);
+  it('Test divide long Number', function(){
+    let result = cal('/',9,9999999999999);
+    assert.equal(result,9.0000000000009e-13);
+  });
+
+  it('Test multiply long Number', function(){
+    let result = cal('*',999999999999,9999999999999);
+    assert.equal(result,9.999999999989e+24);
+  });
+
+  /*
+   *  Non - Funtional Test Cases
+   */
+
+  it('Return Type of Result should be Number', function(){
+    let result = cal('*',999999999999,9999999999999);
     assert.typeOf(result,'number');
   });
 
+  it('Test if invalid input return the string error message', function(){
+    let result = cal('+','a',-3);
+    assert.typeOf(result,'string');
+  });
+
+
+  /*
+   * Non Functional Negative / Explorative / Adhoc Test Cases
+   */
+
   it('Test invalid one input numbers', function(){
     let result = cal('+','a',-3);
-    assert.equal(result,'Invalid Input Numbers');
+    assert.equal(result,'Invalid Input Number');
   });
 
   it('Test Null input number', function(){
-    let result = cal('+',5,'');
+    let result = cal('+',5,);
     assert.equal(result,'One or more of the inputs to the function is Empty / Null, Input format --> calculate('+', 4 7)');
   });
-  it('Test Null input number', function(){
+  it('Test Null input operator', function(){
     let result = cal('',5,8);
     assert.equal(result,'One or more of the inputs to the function is Empty / Null, Input format --> calculate('+', 4 7)');
   });
 
   it('Test invalid input numbers', function(){
-    let result = cal('+','a', 'b');
-    assert.equal(result,'Invalid Input Numbers');
+    let result = cal('+','a','b');
+    assert.equal(result,'Invalid Input Number');
+  });
+
+  it('Test invalid input numbers', function(){
+    let result = cal('+','4','6');
+    assert.equal(result,'Invalid Input Number');
+  });
+
+  it('Test invalid input number', function(){
+    let result = cal('+',null,8);
+    assert.equal(result,'One or more of the inputs to the function is Empty / Null, Input format --> calculate('+', 4 7)');
+  });
+
+  it('Test Null input operator', function(){
+    let result = cal('null',8,8);
+    assert.equal(result,'Invalid Input Operator');
   });
 
   it('Test invalid input operator', function(){
